ab37708a5e9379ce9ad769a2800dbfa6983975c3
Grzegorz Gogolewski <grzegorz.gogolewski@gmail.com>
2019-02-07 11:20:34 +0100

    Inicjalizacja

--- Messenger/FormMain.Designer.cs
+++ Messenger/FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Messenger
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FormMain";
+            this.Text = "Messenger";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
---
+++ Messenger/FormMain.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace Messenger
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+
+            CefSettings settings = new CefSettings
+            {
+                CachePath = AppDomain.CurrentDomain.BaseDirectory + @"\cache",
+                Locale = "pl"
+            };
+
+            Cef.EnableHighDPISupport();
+            CefSharpSettings.ShutdownOnExit = true;
+
+            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+
+            TabControl tabControl = new TabControl
+            {
+                Dock = DockStyle.Fill,
+                Alignment = TabAlignment.Bottom,
+                SizeMode = TabSizeMode.Fixed,
+                ItemSize = new Size(100, 25)
+            };
+
+            TabPage pageMessenger = new TabPage("Messenger");
+            tabControl.TabPages.Add(pageMessenger);
+
+            ChromiumWebBrowser browserMessenger = new ChromiumWebBrowser("https://www.messenger.com");
+            browserMessenger.Dock = DockStyle.Fill;
+            pageMessenger.Controls.Add(browserMessenger);
+
+            TabPage pageMessages = new TabPage("Wiadomości");
+            tabControl.TabPages.Add(pageMessages);
+
+            ChromiumWebBrowser browserMessages = new ChromiumWebBrowser("https://messages.android.com/");
+            browserMessages.Dock = DockStyle.Fill;
+            pageMessages.Controls.Add(browserMessages);
+
+            TabPage pageWhatsapp = new TabPage("WhatsApp");
+            tabControl.TabPages.Add(pageWhatsapp);
+
+            ChromiumWebBrowser browserWhatsapp = new ChromiumWebBrowser("https://web.whatsapp.com/");
+            browserWhatsapp.Dock = DockStyle.Fill;
+            pageWhatsapp.Controls.Add(browserWhatsapp);
+
+            Controls.Add(tabControl);
+
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+    }
+}
