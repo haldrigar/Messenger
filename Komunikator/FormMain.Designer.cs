9c133acef2462765f5ddc59e0d17e56783957f67
Grzegorz Gogolewski <grzegorz.gogolewski@gmail.com>
2019-02-20 15:25:34 +0100

    Dodany Slack Dodana ikona Poprawki aktywności okien

--- Komunikator/FormMain.Designer.cs
+++ Komunikator/FormMain.Designer.cs
@@ -28,11 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.tabControl = new System.Windows.Forms.TabControl();
             this.tabPageMessenger = new System.Windows.Forms.TabPage();
             this.tabPageGG = new System.Windows.Forms.TabPage();
             this.tabPageWhatsApp = new System.Windows.Forms.TabPage();
             this.tabPageSkype = new System.Windows.Forms.TabPage();
+            this.tabPageSlackOpgk = new System.Windows.Forms.TabPage();
             this.tabControl.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -43,63 +45,76 @@
             this.tabControl.Controls.Add(this.tabPageGG);
             this.tabControl.Controls.Add(this.tabPageWhatsApp);
             this.tabControl.Controls.Add(this.tabPageSkype);
+            this.tabControl.Controls.Add(this.tabPageSlackOpgk);
             this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl.ItemSize = new System.Drawing.Size(100, 25);
             this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Margin = new System.Windows.Forms.Padding(0);
             this.tabControl.Name = "tabControl";
+            this.tabControl.Padding = new System.Drawing.Point(0, 0);
             this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(1092, 719);
+            this.tabControl.Size = new System.Drawing.Size(1022, 719);
             this.tabControl.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
             this.tabControl.TabIndex = 0;
+            this.tabControl.Click += new System.EventHandler(this.TabControl_Click);
             // 
             // tabPageMessenger
             // 
+            this.tabPageMessenger.BackColor = System.Drawing.Color.Transparent;
             this.tabPageMessenger.Location = new System.Drawing.Point(4, 4);
+            this.tabPageMessenger.Margin = new System.Windows.Forms.Padding(0);
             this.tabPageMessenger.Name = "tabPageMessenger";
-            this.tabPageMessenger.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageMessenger.Size = new System.Drawing.Size(1084, 686);
+            this.tabPageMessenger.Size = new System.Drawing.Size(1014, 686);
             this.tabPageMessenger.TabIndex = 0;
             this.tabPageMessenger.Text = "Messenger";
-            this.tabPageMessenger.UseVisualStyleBackColor = true;
             // 
             // tabPageGG
             // 
+            this.tabPageGG.ForeColor = System.Drawing.SystemColors.ControlText;
             this.tabPageGG.Location = new System.Drawing.Point(4, 4);
+            this.tabPageGG.Margin = new System.Windows.Forms.Padding(0);
             this.tabPageGG.Name = "tabPageGG";
-            this.tabPageGG.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageGG.Size = new System.Drawing.Size(1084, 686);
+            this.tabPageGG.Size = new System.Drawing.Size(1014, 686);
             this.tabPageGG.TabIndex = 1;
             this.tabPageGG.Text = "GG";
-            this.tabPageGG.UseVisualStyleBackColor = true;
             // 
             // tabPageWhatsApp
             // 
             this.tabPageWhatsApp.Location = new System.Drawing.Point(4, 4);
+            this.tabPageWhatsApp.Margin = new System.Windows.Forms.Padding(0);
             this.tabPageWhatsApp.Name = "tabPageWhatsApp";
-            this.tabPageWhatsApp.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageWhatsApp.Size = new System.Drawing.Size(1084, 686);
+            this.tabPageWhatsApp.Size = new System.Drawing.Size(1014, 686);
             this.tabPageWhatsApp.TabIndex = 2;
             this.tabPageWhatsApp.Text = "WhatsApp";
-            this.tabPageWhatsApp.UseVisualStyleBackColor = true;
             // 
             // tabPageSkype
             // 
             this.tabPageSkype.Location = new System.Drawing.Point(4, 4);
+            this.tabPageSkype.Margin = new System.Windows.Forms.Padding(0);
             this.tabPageSkype.Name = "tabPageSkype";
-            this.tabPageSkype.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageSkype.Size = new System.Drawing.Size(1084, 686);
+            this.tabPageSkype.Size = new System.Drawing.Size(1014, 686);
             this.tabPageSkype.TabIndex = 3;
             this.tabPageSkype.Text = "Skype";
-            this.tabPageSkype.UseVisualStyleBackColor = true;
+            // 
+            // tabPageSlackOpgk
+            // 
+            this.tabPageSlackOpgk.Location = new System.Drawing.Point(4, 4);
+            this.tabPageSlackOpgk.Margin = new System.Windows.Forms.Padding(0);
+            this.tabPageSlackOpgk.Name = "tabPageSlackOpgk";
+            this.tabPageSlackOpgk.Size = new System.Drawing.Size(1014, 686);
+            this.tabPageSlackOpgk.TabIndex = 4;
+            this.tabPageSlackOpgk.Text = "Slack OPGK";
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1092, 719);
+            this.ClientSize = new System.Drawing.Size(1022, 719);
             this.Controls.Add(this.tabControl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormMain";
             this.Text = "Komunikator";
+            this.Activated += new System.EventHandler(this.FormMain_Activated);
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
             this.Load += new System.EventHandler(this.FormMain_Load);
             this.tabControl.ResumeLayout(false);
@@ -114,7 +129,7 @@
         private System.Windows.Forms.TabPage tabPageGG;
         private System.Windows.Forms.TabPage tabPageWhatsApp;
         private System.Windows.Forms.TabPage tabPageSkype;
-
+        private System.Windows.Forms.TabPage tabPageSlackOpgk;
     }
 }
 
--- Komunikator/FormMain.cs
+++ Komunikator/FormMain.cs
@@ -1,32 +1,60 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using CefSharp;
 using CefSharp.WinForms;
+using CefSharp.WinForms.Internals;
 using Komunikator.Handlers;
 
 namespace Komunikator
 {
     public partial class FormMain : Form
     {
-        readonly ChromiumWebBrowser _browserMessenger;
-        private bool _browserMessengerInitialized = false;
+        [DllImport("user32.dll")]
+        static extern bool FlashWindow(IntPtr hwnd, bool bInvert);
 
-        readonly ChromiumWebBrowser _browserGg;
-        private bool _browserGgInitialized = false;
+        ChromiumWebBrowser _browserMessenger;
+        private bool _browserMessengerInitialized;
+        private bool _tabMessengerUnread;
 
-        readonly ChromiumWebBrowser _browserWhatsapp;
-        private bool _browserWhatsappInitialized = false;
+        ChromiumWebBrowser _browserGg;
+        private bool _browserGgInitialized;
+        private bool _tabGgUnread;
+
+        ChromiumWebBrowser _browserWhatsapp;
+        private bool _browserWhatsappInitialized;
+        private bool _tabWhatsappUnread;
+
+        ChromiumWebBrowser _browserSkype;
+        private bool _browserSkypeInitialized;
+        private bool _tabSkypeUnread;
+
+        ChromiumWebBrowser _browserSlackOpgk;
+        private bool _browserSlackOpgkInitialized;
+        private bool _tabSlackOpgkUnread;
+
+
 
         public FormMain()
         {
+            InitializeChromium();
 
+            InitializeComponent();
+        }
+
+        private void InitializeChromium()
+        {
             CefSettings settings = new CefSettings
             {
                 CachePath = AppDomain.CurrentDomain.BaseDirectory + @"\cache",
                 Locale = "pl"
             };
 
+            settings.CefCommandLineArgs.Add("enable-media-stream", "1");
+
+
             Cef.EnableHighDPISupport();
+
 
             Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
 
@@ -38,7 +66,9 @@ namespace Komunikator
                 DownloadHandler = new DownloadHandler(),
                 RequestHandler = new RequestHandler()
             };
+
             _browserMessenger.LoadingStateChanged += BrowserMessenger_LoadingStateChanged;
+            _browserMessenger.TitleChanged += OnBrowserMessengerTitleChanged;
 
             // --------------------------------------------------------------------------------------------------------------------------------------
 
@@ -48,7 +78,9 @@ namespace Komunikator
                 DownloadHandler = new DownloadHandler(),
                 RequestHandler = new RequestHandler()
             };
+
             _browserGg.LoadingStateChanged += BrowserGg_LoadingStateChanged;
+            _browserGg.TitleChanged += OnBrowserGgTitleChanged;
 
             // --------------------------------------------------------------------------------------------------------------------------------------
 
@@ -58,11 +90,31 @@ namespace Komunikator
                 DownloadHandler = new DownloadHandler(),
                 RequestHandler = new RequestHandler()
             };
+
             _browserWhatsapp.LoadingStateChanged += BrowserWhatsapp_LoadingStateChanged;
+            _browserWhatsapp.TitleChanged += OnBrowserWhatsappTitleChanged;
 
             // --------------------------------------------------------------------------------------------------------------------------------------
 
-            InitializeComponent();
+            _browserSkype = new ChromiumWebBrowser("https://preview.web.skype.com")
+            {
+                Dock = DockStyle.Fill,
+                DownloadHandler = new DownloadHandler(),
+                RequestHandler = new RequestHandler()
+            };
+            _browserSkype.LoadingStateChanged += BrowserSkype_LoadingStateChanged;
+            _browserSkype.TitleChanged += OnBrowserSkypeTitleChanged;
+
+            // --------------------------------------------------------------------------------------------------------------------------------------
+
+            _browserSlackOpgk = new ChromiumWebBrowser("https://opgkgdansk.slack.com")
+            {
+                Dock = DockStyle.Fill,
+                DownloadHandler = new DownloadHandler(),
+                RequestHandler = new RequestHandler()
+            };
+            _browserSlackOpgk.LoadingStateChanged += BrowserSlackOpgk_LoadingStateChanged;
+            _browserSlackOpgk.TitleChanged += OnBrowserSlackOpgkTitleChanged;
         }
 
         private void BrowserMessenger_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
@@ -74,8 +126,21 @@ namespace Komunikator
 
                 if (_browserGgInitialized == false)
                 {
-                    tabControl.Invoke((MethodInvoker) (() => tabControl.SelectTab("tabPageGG")));
+                    tabControl.InvokeOnUiThreadIfRequired(() => tabControl.SelectTab("tabPageGG"));
                 }
+            }
+        }
+
+        private void OnBrowserMessengerTitleChanged(object sender, TitleChangedEventArgs args)
+        {
+            if (!args.Title.Equals("Messenger") && _tabMessengerUnread == false && _browserMessengerInitialized && !tabPageMessenger.IsActiveControl())
+            {
+                _tabMessengerUnread = true;
+
+                tabPageMessenger.InvokeOnUiThreadIfRequired(() => tabPageMessenger.Text = "Messenger (+)");
+
+                this.InvokeOnUiThreadIfRequired(() => FlashWindow(Handle, true));
+
             }
         }
 
@@ -87,8 +152,20 @@ namespace Komunikator
 
                 if (_browserWhatsappInitialized == false)
                 {
-                    tabControl.Invoke((MethodInvoker) (() => tabControl.SelectTab("tabPageWhatsApp")));
+                    tabControl.InvokeOnUiThreadIfRequired(() => tabControl.SelectTab("tabPageWhatsApp"));
                 }
+            }
+        }
+
+        private void OnBrowserGgTitleChanged(object sender, TitleChangedEventArgs args)
+        {
+            if (!args.Title.Equals("GG") && _tabGgUnread == false && _browserGgInitialized && !this.IsActiveControl())
+            {
+                _tabGgUnread = true;
+
+                tabPageGG.InvokeOnUiThreadIfRequired(() => tabPageGG.Text = "GG (+)");
+
+                this.InvokeOnUiThreadIfRequired(() => FlashWindow(Handle, true));
             }
         }
 
@@ -98,10 +175,72 @@ namespace Komunikator
             {
                 _browserWhatsappInitialized = true;
 
+                if (_browserSkypeInitialized == false)
+                {
+                    tabControl.InvokeOnUiThreadIfRequired(() => tabControl.SelectTab("tabPageSkype"));
+                }
+            }
+        }
+
+        private void OnBrowserWhatsappTitleChanged(object sender, TitleChangedEventArgs args)
+        {
+            if (!args.Title.Equals("WhatsApp") && _tabWhatsappUnread == false && _browserWhatsappInitialized && !this.IsActiveControl())
+            {
+                _tabWhatsappUnread = true;
+
+                tabPageWhatsApp.InvokeOnUiThreadIfRequired(() => tabPageWhatsApp.Text = "WhatsApp (+)");
+
+                this.InvokeOnUiThreadIfRequired(() => FlashWindow(Handle, true));
+            }
+        }
+
+        private void BrowserSkype_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+            if (e.IsLoading == false)
+            {
+                _browserSkypeInitialized = true;
+
+                if (_browserSlackOpgkInitialized == false)
+                {
+                    tabControl.InvokeOnUiThreadIfRequired(() => tabControl.SelectTab("tabPageSlackOpgk"));
+                }
+            }
+        }
+
+        private void OnBrowserSkypeTitleChanged(object sender, TitleChangedEventArgs args)
+        {
+            if (!args.Title.Equals("Skype") && _tabSkypeUnread == false && _browserSkypeInitialized && !this.IsActiveControl())
+            {
+                _tabSkypeUnread = true;
+
+                tabPageSkype.InvokeOnUiThreadIfRequired(() => tabPageSkype.Text = "Skype (+)");
+
+                this.InvokeOnUiThreadIfRequired(() => FlashWindow(Handle, true));
+            }
+        }
+
+        private void BrowserSlackOpgk_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+            if (e.IsLoading == false)
+            {
+                _browserSlackOpgkInitialized = true;
+
                 if (_browserMessengerInitialized == false)
                 {
-                    tabControl.Invoke((MethodInvoker) (() => tabControl.SelectTab("tabPageMessenger")));
+                    tabControl.InvokeOnUiThreadIfRequired(() => tabControl.SelectTab("tabPageMesseneger"));
                 }
+            }
+        }
+
+        private void OnBrowserSlackOpgkTitleChanged(object sender, TitleChangedEventArgs args)
+        {
+            if (args.Title.StartsWith("!") && _tabSlackOpgkUnread == false && _browserSlackOpgkInitialized && !this.IsActiveControl())
+            {
+                _tabSlackOpgkUnread = true;
+
+                tabPageSlackOpgk.InvokeOnUiThreadIfRequired(() => tabPageSlackOpgk.Text = "Slack OPGK (+)");
+
+                this.InvokeOnUiThreadIfRequired(() => FlashWindow(Handle, true));
             }
         }
 
@@ -112,11 +251,80 @@ namespace Komunikator
             tabPageGG.Controls.Add(_browserGg);
 
             tabPageWhatsApp.Controls.Add(_browserWhatsapp);
+
+            tabPageSkype.Controls.Add(_browserSkype);
+
+            tabPageSlackOpgk.Controls.Add(_browserSlackOpgk);
         }
 
         private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
         {
             Cef.Shutdown();
+        }
+
+        private void TabControl_Click(object sender, EventArgs e)
+        {
+            switch (tabControl.SelectedTab.Name)
+            {
+                case "tabPageMessenger":
+                    tabPageMessenger.Text = "Messenger";
+                    _tabMessengerUnread = false;
+                    break;
+
+                case "tabPageGG":
+                    tabPageGG.Text = "GG";
+                    _tabGgUnread = false;
+                    break;
+
+                case "tabPageWhatsApp":
+                    tabPageWhatsApp.Text = "WhatsApp";
+                    _tabWhatsappUnread = false;
+                    break;
+
+                case "tabPageSkype":
+                    tabPageSkype.Text = "Skype";
+                    _tabSkypeUnread = false;
+                    break;
+
+                case "tabPageSlackOpgk":
+                    tabPageSlackOpgk.Text = "Slack OPGK";
+                    _tabSlackOpgkUnread = false;
+                    break;
+            }
+        }
+
+        private void FormMain_Activated(object sender, EventArgs e)
+        {
+            if (_browserMessengerInitialized && _browserGgInitialized && _browserWhatsappInitialized && _browserSkypeInitialized)
+            {
+                switch (tabControl.SelectedTab.Name)
+                {
+                    case "tabPageMessenger":
+                        tabPageMessenger.Text = "Messenger";
+                        _tabMessengerUnread = false;
+                        break;
+
+                    case "tabPageGG":
+                        tabPageGG.Text = "GG";
+                        _tabGgUnread = false;
+                        break;
+
+                    case "tabPageWhatsApp":
+                        tabPageWhatsApp.Text = "WhatsApp";
+                        _tabWhatsappUnread = false;
+                        break;
+
+                    case "tabPageSkype":
+                        tabPageSkype.Text = "Skype";
+                        _tabSkypeUnread = false;
+                        break;
+
+                    case "tabPageSlackOpgk":
+                        tabPageSlackOpgk.Text = "Slack OPGK";
+                        _tabSlackOpgkUnread = false;
+                        break;
+                }
+            }
         }
     }
 }
